fcb792dd060857860e7bd69d1a96433e3d86dad2
JeidyOlaya <jeidkata@gmail.com>
2025-11-18 12:09:47 -0500

    feat: defino mi modelo de datos Product.

--- API_Product/Models/product.cs
+++ API_Product/Models/product.cs
@@ -1,6 +1,11 @@
 ﻿namespace API_Product.Models
 {
-    public class product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
