9d3015d9a0cc36267e55d36a887362627ee41342
JeidyOlaya <jeidkata@gmail.com>
2025-11-19 11:19:53 -0500

    build: Reemplazo todo lo relacionado con OpenApi para que se use el Swagger. UseSwaggerUI muestra todos los endpoints.

--- API_Product/Program.cs
+++ API_Product/Program.cs
@@ -1,17 +1,34 @@
+using Microsoft.Data.Sqlite;
+using API_Product.Repositories;
+using System.Data;
+using Dapper;
+using NuGet.DependencyResolver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+
+
+// Configure Dapper and SQLite repository
+builder.Services.AddTransient<IDbConnection>(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    return new SqliteConnection(connectionString);
+});
+
+// Register the ProductRepository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
